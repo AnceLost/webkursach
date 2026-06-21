94599dd4c1070192a03146bc0eaf83c860c42a01
AnceLost <jayseven228@yandex.ru>
2026-06-21 12:23:12 +0300

    add sqlite support

--- app/config.py
+++ app/config.py
@@ -3,12 +3,16 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DB_HOST: str = "localhost"
     DB_PORT: int = 5432
-    DB_USER: str = "gamevault_user"
+    DB_USER: str = "user"
     DB_PASS: str = "secret"
-    DB_NAME: str = "gamevault"
+    DB_NAME: str = "db_name"
+    
+    SQLITE_DB_URI: str = "sqlite:///:memory:"
+    
     FLASK_SECRET_KEY: str = "default-secret-key"
     
     TESTING: bool = False
+    USING_SQLITE: bool = False
     WTF_CSRF_ENABLED: bool = True      
     
     @property
@@ -19,6 +23,8 @@ class Settings(BaseSettings):
     def SQLALCHEMY_DATABASE_URI(self):
         if self.TESTING:
             return "sqlite:///:memory:"
+        if self.USING_SQLITE:
+            return self.SQLITE_DB_URI
         return f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}/{self.DB_NAME}"
 
     model_config = SettingsConfigDict(env_file="app/.env")
